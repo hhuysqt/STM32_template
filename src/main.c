6777fdfa2f6bcf4b434710c4ad7bda47734a250d
hyq9606 <hyq9606@126.com>
2016-02-24 23:10:53 +0800

    Template code

---
+++ src/main.c
@@ -0,0 +1,59 @@
+#include <stm32f10x.h>
+#include <stm32f10x_rcc.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_tim.h>
+
+#define LED_PIN GPIO_Pin_0
+#define LED_ON GPIO_ResetBits(GPIOB, LED_PIN);
+#define LED_OFF GPIO_SetBits(GPIOB, LED_PIN);
+
+void InitWorld(void)
+{
+	TIM_TimeBaseInitTypeDef tim2_base;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	
+	TIM_DeInit(TIM2);
+	tim2_base.TIM_Prescaler = 71;
+	tim2_base.TIM_CounterMode = TIM_CounterMode_Up;
+	tim2_base.TIM_ClockDivision = 0;
+	tim2_base.TIM_Period = 1000; 
+	TIM_TimeBaseInit(TIM2, &tim2_base);
+	TIM_Cmd(TIM2, ENABLE);
+}
+
+void delay_ms(u16 n)
+{
+	while(n--)
+	{
+		while(TIM_GetFlagStatus(TIM2, TIM_FLAG_Update) != SET);
+		TIM_ClearFlag(TIM2, TIM_FLAG_Update);
+	}
+}
+
+int main()
+{
+	InitWorld();
+
+	{
+		GPIO_InitTypeDef LEDIO;
+		
+		RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+		
+		LEDIO.GPIO_Mode = GPIO_Mode_Out_OD;
+		LEDIO.GPIO_Speed = GPIO_Speed_50MHz;
+		LEDIO.GPIO_Pin = LED_PIN;
+		
+		GPIO_Init(GPIOB, &LEDIO);
+	}
+
+	while(1)
+	{
+		delay_ms(500);
+		LED_ON;
+		delay_ms(500);
+		LED_OFF;
+	}
+	
+	return 0;
+}
